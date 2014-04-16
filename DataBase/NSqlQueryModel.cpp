b31227d509eae8ce5c9b9856b0ec57ea413d79eb
Nakim <mawet.xavier@gmail.com>
2014-04-17 01:51:53 +0200

    Changed tableViewLapRanking edit triggers and selection mode

--- DataBase/NSqlQueryModel.cpp
+++ DataBase/NSqlQueryModel.cpp
@@ -9,4 +9,9 @@ NSqlQueryModel::NSqlQueryModel(QObject* parent) :
 void NSqlQueryModel::refresh(void)
 {
     this->setQuery(this->query().executedQuery());
+
+    QModelIndex topLeft = this->index(0, 0);
+    QModelIndex bottomRight = this->index(
+                this->rowCount() - 1, this->columnCount() - 1);
+    emit dataChanged(topLeft, bottomRight);
 }
